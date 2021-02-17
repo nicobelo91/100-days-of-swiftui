0af5626b5bdae48258cab3bf24d4779687f0409c
Nicolas Cobelo <nicolascobelo@The-App-Master.local>
2021-02-17 20:50:25 -0300

    Continue working on the Milestone project

--- 05-Milestone-Projects1-3/RockPaperScissors/ContentView.swift
+++ 05-Milestone-Projects1-3/RockPaperScissors/ContentView.swift
@@ -8,27 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var rps = ["👊", "✋", "✌️"]
+    @State var rockPaperScissors = ["👊", "✋", "✌️"]
     @State private var appsChoice = Int.random(in: 0...2)
+    
     @State private var shouldWin = Bool.random()
+    var result: String {
+        if shouldWin {
+            return "win"
+        } else {
+            return "lose"
+        }
+    }
     @State private var userScore = 0
     var body: some View {
         VStack {
-            Text("Player's Score")
-            Text("App's move")
-            Text("Should I win or Lose?")
+            Text("App's choice")
+            Text(rockPaperScissors[appsChoice])
+                .font(.system(size: 90))
+            Text("How to \(result) the game?")
+                .padding()
             HStack {
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        //Action
+                        buttonTapped(number)
                     }) {
-                        Text(self.rps[number])
+                        Text(self.rockPaperScissors[number])
+                            .font(.system(size: 50))
+                            .padding()
                     }
                 }
             }
+            Text("Score \(userScore)")
         }
     }
+    
+    func buttonTapped(_ number: Int) {
+        if shouldWin {
+            if number == (appsChoice + 1) {
+                userScore += 1
+            } else {
+                userScore -= 1
+            }
+        } else {
+            if number == (appsChoice - 1) {
+                userScore += 1
+            } else {
+                userScore -= 1
+            }
+        }
+        appsChoice = Int.random(in: 0...2)
+        shouldWin = Bool.random()
+    }
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
